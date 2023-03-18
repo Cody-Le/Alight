cf10cc8b5c12915ab783199d5eb503934ba88798
Cody Le <49752325+Cody-Le@users.noreply.github.com>
2023-03-18 13:52:56 -0400

    Enemy, Texture
    - Added the enemy chase scene
    - Added a new texture and items, particularly the flowers and doors.

---
+++ Assets/Scrip/Enemy/ToruChase.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ToruChase : MonoBehaviour
+{
+    [SerializeField] private bool chasePlayer = false;
+    [SerializeField] private bool spotPlayer = false;
+    public PlayableDirector timeline;
+    Transform player;
+    [Range(0, 10f)]public float runSpeed = 3f;
+    CharacterController controller;
+    Transform view;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        view = transform.GetChild(0);
+    }
+
+
+    private void Update()
+    {
+        if(player == null) { return; }
+        Vector3 targetPoint = player.position - transform.position;
+
+        if (spotPlayer)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(-targetPoint, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+        }
+
+        if (chasePlayer)
+        {
+            controller.Move(targetPoint.normalized * runSpeed * Time.deltaTime);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            
+            spotPlayer = true;
+            player = other.transform;
+            StartCoroutine(startChase());
+            
+        }
+    }
+
+    
+
+    IEnumerator startChase()
+    {
+        yield return new WaitForSeconds(1.5f);
+        if (!chasePlayer) { timeline.Play(); }
+        chasePlayer = true;
+        
+    }
+
+
+}
