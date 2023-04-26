433739c70e73593ce7735a6bdcbc20b1aa817971
Cody Le <49752325+Cody-Le@users.noreply.github.com>
2023-04-26 11:25:19 -0400

    File Structure
    - Reorganized the file structures for extra producitivity

---
+++ Assets/Script/Enemy/Toru_chase.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ToruChase : MonoBehaviour
+{
+
+
+    [Header("Chase Parameter")]
+    [SerializeField] private bool chasePlayer = false;
+    [SerializeField] private bool spotPlayer = false;
+    Transform player;
+    [Range(0, 10f)]public float runSpeed = 3f;
+    CharacterController controller;
+    public float minDistance;
+
+    [Header("Effects")]
+    public PlayableDirector timeline;
+    public string cutSceneName;
+   
+    public Material retroMaterial;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+   
+        
+    }
+
+
+    private void Update()
+    {
+        if(player == null) { return; }
+        Vector3 targetPoint = player.position - transform.position;
+
+        if (spotPlayer)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(-targetPoint, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+        }
+
+        if (chasePlayer)
+        {
+            controller.Move(targetPoint.normalized * runSpeed * Time.deltaTime);
+            float noiseAmount = 0.008f - targetPoint.magnitude / 100;
+            retroMaterial.SetFloat("NoiseAmount", 0.008f + noiseAmount);
+            if (targetPoint.magnitude < minDistance)
+            {
+                SceneManager.LoadScene(cutSceneName);
+            }
+            
+        }
+
+        
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            
+            spotPlayer = true;
+            player = other.transform;
+            StartCoroutine(startChase());
+            
+        }
+    }
+
+    
+
+    IEnumerator startChase()
+    {
+        yield return new WaitForSeconds(1.5f);
+        if (!chasePlayer) { timeline.Play(); }
+        chasePlayer = true;
+        
+    }
+
+
+}
---
+++ Assets/Script/General/Environment/LevelSpecific/Outside/busInteraction.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class BusInteract : MonoBehaviour
+{
+    public GameEvent onSubtitle;
+    public PlayableDirector playerBus;
+    public GameObject Stranger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            onSubtitle.Raise(this, "[E] to get on bus");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            onSubtitle.Raise(this, "");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                playerBus.Play();
+                Stranger.active = false;
+
+            }
+        }
+    }
+}
